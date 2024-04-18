26d1de7a82d4689b8d5c871e769e4f3760a715d7
zulesss <viktorpostemskij@gmail.com>
2024-04-19 00:28:28 +0300

    solved lab

--- main.cpp
+++ main.cpp
@@ -5,6 +5,12 @@
 
 using namespace std;
 
+void Logger::addRecord(Sphere& sphere)
+{
+    string record = "Object name: " + sphere.objectName + "\nRadius: " + to_string(sphere.radius) ;
+    log += record + "\n";
+}
+
 void Logger::saveLog()
 {
     std::ofstream file("log.txt");
@@ -25,21 +31,15 @@ void Logger::saveLog()
         cout << "Unable to open log file!" << endl;
 }
 
-void Sphere::log(Logger& logger)
-{
-    string record = "Object name: " + objectName + "\nRadius: " + to_string(radius) ;
-    logger.addRecord(record);
-}
-
 int Sphere::instanceCount = 0;
 
 int main() {
     Logger logger;
     
     Sphere sphere1(5.0, "sphere1");
-    sphere1.log(logger);
+    logger.addRecord(sphere1);
     Sphere sphere2(7.0, "sphere2");
-    sphere2.log(logger);
+    logger.addRecord(sphere2);
     
     cout << "Number of Sphere instances: " << Sphere::getInstanceCount() << endl;
     logger.saveLog();
--- main.h
+++ main.h
@@ -2,28 +2,21 @@
 
 using namespace std;
 
-class Logger {
-private:
-    std::string log; 
-
-public:
-    void addRecord(const std::string& record) {log += record + "\n";}
-    void saveLog();
-};
-
 class Sphere {
     
-private:
-    static int instanceCount; 
-    string objectName; 
-    double radius;  
+    static int instanceCount;
 public:
-    Sphere(double r, string name) : radius(r), objectName(name) {instanceCount++;}
-
-    static int getInstanceCount() {return instanceCount;}
-    void setRadius(double r) {radius = r;}
-    double getRadius() const {return radius;}
+    string objectName; 
+    double radius;
     
-    void log(Logger& logger);
+    Sphere(double r, string name) : radius(r), objectName(name) {instanceCount++;}
+    static int getInstanceCount() {return instanceCount;}
 };
 
+class Logger {
+    
+    std::string log; 
+public:
+    void addRecord(Sphere& sphere);
+    void saveLog();
+};
